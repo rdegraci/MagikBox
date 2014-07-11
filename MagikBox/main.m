8588d041e817fdc845f417cf50a40ef4d5259225
Rodney Degracia <rdegraci@elvenlabs.com>
2014-07-11 10:00:54 -0400

    Initial Commit

---
+++ MagikBox/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MagikBox
+//
+//  Created by Rodney Degracia on 7/11/14.
+//  Copyright (c) 2014 Venture Intellectual LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
