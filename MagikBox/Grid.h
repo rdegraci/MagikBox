c5ac5f926b75917d925565a6d2dcd6e03c0b2179
Rodney Degracia <rdegraci@elvenlabs.com>
2014-07-11 19:39:11 -0400

    Add Grid, grid.plist, GridTest to Project

---
+++ MagikBox/Grid.h
@@ -0,0 +1,13 @@
+//
+//  Grid.h
+//  MagikBox
+//
+//  Created by Rodney Degracia on 7/11/14.
+//  Copyright (c) 2014 Venture Intellectual LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Grid : NSObject
+
+@end
---
+++ MagikBox/Grid.m
@@ -0,0 +1,13 @@
+//
+//  Grid.m
+//  MagikBox
+//
+//  Created by Rodney Degracia on 7/11/14.
+//  Copyright (c) 2014 Venture Intellectual LLC. All rights reserved.
+//
+
+#import "Grid.h"
+
+@implementation Grid
+
+@end
